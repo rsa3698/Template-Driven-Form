9e63d54027939df3969d3001d113f74acc27dd40
Raudra SA <raudra.sankar.adak@accenture.com>
2023-10-25 17:09:27 +0530

    ngModel and ngForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'template-driven-form';
   
-  onFormSubmitted(){
-    
+  @ViewChild('registrationForm') form: NgForm
+  onFormSubmitted(form: NgForm){
+    console.log(this.form)
+    console.log(this.form.value)
+    console.log(this.form.value.firstname);
+    console.log(this.form.controls['lastname'].value);
+    console.log(this.form.value.email);
+    console.log(this.form.value.country)
   }
 }
